85622f88c2424932b7543f57592d2d1ddaa765eb
phpinheirobr <pedro.hendrixx@gmail.com>
2022-01-03 14:34:58 -0200

    criação do projeto backend

---
+++ src/main/java/webnext/com/br/cademeupet/CademeupetApplication.java
@@ -0,0 +1,13 @@
+package webnext.com.br.cademeupet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CademeupetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CademeupetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webnext/com/br/cademeupet/CademeupetApplicationTests.java
@@ -0,0 +1,13 @@
+package webnext.com.br.cademeupet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CademeupetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
