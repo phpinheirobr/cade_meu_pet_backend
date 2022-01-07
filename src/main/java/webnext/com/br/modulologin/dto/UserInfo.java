d60e7085a6bfb0b9c1a7351adee0b581e0149e76
phpinheirobr <pedro.hendrixx@gmail.com>
2022-01-07 19:54:00 -0200

    criação de configuração

---
+++ src/main/java/webnext/com/br/modulologin/dto/UserInfo.java
@@ -0,0 +1,11 @@
+package webnext.com.br.modulologin.dto;
+
+import java.util.List;
+
+import lombok.Value;
+
+@Value
+public class UserInfo {
+	private String id, displayName, email;
+	private List<String> roles;
+}
