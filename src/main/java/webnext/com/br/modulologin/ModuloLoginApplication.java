a5e73f112fe935554be136d1cae658ff936431cd
phpinheirobr <pedro.hendrixx@gmail.com>
2022-01-07 16:19:34 -0200

    novo teste

---
+++ src/main/java/webnext/com/br/modulologin/ModuloLoginApplication.java
@@ -0,0 +1,13 @@
+package webnext.com.br.modulologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webnext/com/br/modulologin/ModuloLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package webnext.com.br.modulologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
