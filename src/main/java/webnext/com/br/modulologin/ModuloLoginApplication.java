e7cac634977188d1765edea881c18566efa2c060
phpinheirobr <pedro.hendrixx@gmail.com>
2022-01-14 21:10:47 -0200

    primeiro commit módulo estabilizado

--- src/main/java/webnext/com/br/modulologin/ModuloLoginApplication.java
+++ src/main/java/webnext/com/br/modulologin/ModuloLoginApplication.java
@@ -2,12 +2,24 @@ package webnext.com.br.modulologin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-public class ModuloLoginApplication {
+@SpringBootApplication(scanBasePackages = "webnext.com.br")
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class ModuloLoginApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModuloLoginApplication.class, args);
+		SpringApplicationBuilder app = new SpringApplicationBuilder(ModuloLoginApplication.class);
+		app.run();
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ModuloLoginApplication.class);
 	}
 
 }
